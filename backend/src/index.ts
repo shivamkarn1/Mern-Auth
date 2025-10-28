908e920a9c67095dfce19cee699e15a0b327ab72
shivamkarn <shivamkarn767@gmail.com>
2025-10-28 13:30:22 +0545

    Improve index.ts to improve middleware managing.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,39 @@
 import "dotenv/config";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { connectDB } from "./config/db";
-
+import cors from "cors";
+import { CLIENT_URL } from "../src/constants/env";
+import cookieParser from "cookie-parser";
+import { errorHandler } from "../src/middleware/errorHandler";
+import catchErrors from "./utils/catchErrors";
+import { OK } from "./constants/http";
 const app = express();
 
-app.get("/", (req, res) => {
+//middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+
+app.get("/", (req: Request, res: Response) => {
   return res.status(200).json({
     message: "Server is running Great ;)",
     status: "Healthy ",
   });
 });
-app.get("/health", (_, res) => {
-  return res.status(200).json({
+app.use(cookieParser());
+
+app.get("/health", (_, res: Response) => {
+  return res.status(OK).json({
     status: "Healthy ",
   });
 });
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
 
