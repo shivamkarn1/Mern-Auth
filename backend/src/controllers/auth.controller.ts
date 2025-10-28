a6728fe46095f1f53170c356bd990c288105eaba
shivamkarn <shivamkarn767@gmail.com>
2025-10-28 13:36:44 +0545

    Add registration route and handler for user registration

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,9 @@
+import catchErrors from "../utils/catchErrors";
+
+const registerHandler = catchErrors(async (req, res) => {
+  // validate request
+  // call service
+  // return response
+});
+
+export { registerHandler };
---
+++ backend/src/routes/auth.route.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { registerHandler } from "../controllers/auth.controller";
+const authRoute = Router();
+
+// the prefix is : /auth
+
+authRoute.post("/register", registerHandler);
