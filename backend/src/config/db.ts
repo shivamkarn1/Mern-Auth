b00bfc04eece35514233ef72340a602b1a4f96eb
shivamkarn <shivamkarn767@gmail.com>
2025-10-28 12:38:50 +0545

    Made a few changes.

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,7 +3,8 @@ import { MONGO_URI } from "../constants/env";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(MONGO_URI);
-    console.log("The connection instance is : ", connectionInstance);
+    // console.log("The connection instance is : ", connectionInstance);
+    console.log("MongoDB Connected Successfully 🌿");
   } catch (error) {
     console.log("MongoDB connection Error : ", error);
     process.exit(1);
