554519a8aa9cc499734ffbc3bd167ad7bb5e9c24
shivamkarn <shivamkarn767@gmail.com>
2025-10-29 23:08:56 +0545

    Add 15mins expiry function.

--- backend/src/utils/date.ts
+++ backend/src/utils/date.ts
@@ -1,5 +1,12 @@
 const oneYearFromNow = () => {
-  new Date(Date.now() + 365 * 24 * 60 * 1000);
+  return new Date(Date.now() + 365 * 24 * 60 * 60 * 1000);
 };
 
-export { oneYearFromNow };
+const thirtyDaysFromNow = () => {
+  return new Date(Date.now() + 30 * 24 * 60 * 60 * 1000);
+};
+const fifteenMinutesFromNow = () => {
+  return new Date(Date.now() + 15 * 60 * 1000);
+};
+
+export { oneYearFromNow, thirtyDaysFromNow, fifteenMinutesFromNow };
