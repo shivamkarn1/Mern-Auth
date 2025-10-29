c674ee365e9fae756691514dd166a4520ae3b748
shivamkarn <shivamkarn767@gmail.com>
2025-10-29 23:12:32 +0545

    Completed Registration process.

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 import catchErrors from "../utils/catchErrors";
 import { z } from "zod";
-
+import { createAccount } from "../services/auth.service";
+import { CREATED } from "../constants/http";
+import { setAuthCookies } from "../utils/setAuthCookies";
 const registerSchema = z
   .object({
     email: z.string().email().min(1).max(255),
@@ -24,8 +26,10 @@ const registerHandler = catchErrors(
         : req.headers["user-agent"],
     });
     // call service
-
-    // return response
+    const { user, accessToken, refreshToken } = await createAccount(request);
+    return setAuthCookies({ res, accessToken, refreshToken })
+      .status(CREATED)
+      .json({ message: "User Registered Successfully", user });
   }
 );
 
