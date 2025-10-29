24f77afba60dcaeed43162541e3072d6d774d4cb
shivamkarn <shivamkarn767@gmail.com>
2025-10-29 23:11:16 +0545

    Add Session model with schema and interface for session management

---
+++ backend/src/models/session.model.ts
@@ -0,0 +1,30 @@
+import mongoose, { Document, Schema, model } from "mongoose";
+import { thirtyDaysFromNow } from "../utils/date";
+export interface SessionDocument extends Document {
+  userId: mongoose.Types.ObjectId;
+  userAgent?: string;
+  createdAt: Date;
+  expiresAt: Date | undefined;
+}
+
+const sessionSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    index: true,
+  },
+  userAgent: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  expiresAt: {
+    type: Date,
+    default: thirtyDaysFromNow,
+  },
+});
+
+export const SessionModal = model("SessionModal", sessionSchema);
