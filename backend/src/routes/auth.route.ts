c56f73121f9e56c19774bb62bf8265106ef8d77b
shivamkarn <shivamkarn767@gmail.com>
2025-10-30 12:35:10 +0545

    Added login route.

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { registerHandler } from "../controllers/auth.controller";
+import { registerHandler, loginHandler } from "../controllers/auth.controller";
 const authRoute = Router();
 
 // the prefix is : /auth
 
 authRoute.post("/register", registerHandler);
+authRoute.post("/login", loginHandler);
 
 export default authRoute;
