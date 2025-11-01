9fe4587558a9f37274d82c39694c5713dd37d6f4
shivamkarn <shivamkarn767@gmail.com>
2025-11-01 22:11:57 +0545

    Added logout route.

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { registerHandler, loginHandler } from "../controllers/auth.controller";
+import {
+  registerHandler,
+  loginHandler,
+  logoutHandler,
+} from "../controllers/auth.controller";
 const authRoute = Router();
 
 // the prefix is : /auth
 
 authRoute.post("/register", registerHandler);
 authRoute.post("/login", loginHandler);
+authRoute.post("/logout", logoutHandler);
 
 export default authRoute;
