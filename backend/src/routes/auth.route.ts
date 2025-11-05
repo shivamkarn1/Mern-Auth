75efea742a861537f3cf9686499d42f2186fccd4
shivamkarn <shivamkarn767@gmail.com>
2025-11-05 08:45:08 +0545

    Added verifyemail route.

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -4,6 +4,7 @@ import {
   loginHandler,
   logoutHandler,
   refreshHandler,
+  verifyEmailHandler,
 } from "../controllers/auth.controller";
 const authRoute = Router();
 
@@ -13,5 +14,6 @@ authRoute.post("/register", registerHandler);
 authRoute.post("/login", loginHandler);
 authRoute.get("/refresh", refreshHandler);
 authRoute.post("/logout", logoutHandler);
+authRoute.get("/email/verify/:code", verifyEmailHandler);
 
 export default authRoute;
